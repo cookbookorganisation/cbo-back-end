e287eb1a388ae3880484157cd23f0c1bc3a941e1
Michael Callahan <CallahanMichaelE@gmail.com>
2020-10-29 15:42:18 -0600

    added in first CRUD function for users

--- users/users-model.js
+++ users/users-model.js
@@ -2,4 +2,15 @@ const dB = require('../database/dbConfig.js');
 
 module.exports = {
     // names of functions
+    find,
 };
+
+// Functions
+
+function find(){
+    return dB('users')
+}
+
+function findBy(filter){
+    return dB('users').where(filter)
+}
--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
+const Users = require('./users-model.js')
 
+// CRUDs Here! Start with /api/users
 
-
+router.get('/', async (req, res) => {
+    await Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(error => {
+        res.status(500).json({ message: "failed to pull users from server", error: error.message})
+    })
+})
 
 module.exports = router;
