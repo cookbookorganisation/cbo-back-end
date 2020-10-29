c5cdea7b0652048f7f3f6c59e3c2d63f5a1ec1e6
Michael Callahan <CallahanMichaelE@gmail.com>
2020-10-29 15:32:07 -0600

    updated server with users route

--- api/server.js
+++ api/server.js
@@ -6,6 +6,11 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+const usersRouter = require('../users/users-router.js')
+
+
+server.use('/api/users', usersRouter);
+
 server.get("/", (req, res) => {
   res.status(200).json("Hello world!");
 });
